cf4d934894b08a7cf203924834d297d24066692d
Yura Kivlyuk <kivlyukyura@gmail.com>
2024-12-04 00:33:10 +0200

    .

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+
 const PORT = process.env.PORT || 80;
 
 app.use(express.static(path.join(__dirname, 'views')));
